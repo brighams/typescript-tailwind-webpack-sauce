119118cf63bba76824038bbb3e12c2037f79094f
Brigham <brigham@starkeeper.io>
2025-12-31 13:41:45 -0800

    index.html tab title

--- app-sauce.sh
+++ app-sauce.sh
@@ -210,7 +210,7 @@ cat > src/index.html << 'EOL'
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>StarPlayer</title>
+  <title>App SAUCE STARTER</title>
   <!-- Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
